defa225630f013cd7ea751a923d1cf6717ef8101
Victor Escudero <victor.escudero@bestsecret.com>
2021-01-31 14:27:45 +0100

    UI: Created Lives visualizer in the ui and logic to update it

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -62,6 +62,7 @@ public class Player : MonoBehaviour
         }
 
         _lives--;
+        _uiManager.UpdateLives(_lives);
 
         if (_lives < 1)
         {
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,13 +6,25 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Text _scoreText;
 
+    [SerializeField]
+    private Image _livesImg;
+
+    [SerializeField]
+    private Sprite[] _liveSprites;
+    
     void Start()
     {
+
         _scoreText.text = "Score: " + 0;
     }
 
     public void UpdateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore;
+    }
+
+    public void UpdateLives(int currentLives)
+    {
+        _livesImg.sprite = _liveSprites[currentLives];
     }
 }
