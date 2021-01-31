11a0eb6f42beea56a8c4ab37efa7cdd2a5b542ca
Victor Escudero <victor.escudero@bestsecret.com>
2021-01-31 14:52:14 +0100

    Added Flicker effect to the GameOver text.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -8,6 +9,9 @@ public class UIManager : MonoBehaviour
 
     [SerializeField]
     private Text _gameOverText;
+
+    [SerializeField]
+    private Text _restartText;
 
     [SerializeField]
     private Image _livesImg;
@@ -20,6 +24,7 @@ public class UIManager : MonoBehaviour
 
         _scoreText.text = "Score: " + 0;
         _gameOverText.gameObject.SetActive(false);
+        _restartText.gameObject.SetActive(false);
     }
 
     public void UpdateScore(int playerScore)
@@ -34,6 +39,19 @@ public class UIManager : MonoBehaviour
         if (currentLives == 0)
         {
             _gameOverText.gameObject.SetActive(true);
+            _restartText.gameObject.SetActive(true);
+            StartCoroutine(GameOverFlickerRoutine());
+        }
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = string.Empty;
+            yield return new WaitForSeconds(0.5f);
         }
     }
 }
