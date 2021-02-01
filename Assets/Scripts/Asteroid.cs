118b38c78244343246d8a558d1747a9c39d49563
Victor Escudero <victor.escudero@bestsecret.com>
2021-02-01 21:36:39 +0100

    Created an Asteroid and make it rotate

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    //[SerializeField]
+    //private float _speed = 2f;
+
+    [SerializeField]
+    private float _rotateSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    }
+}
