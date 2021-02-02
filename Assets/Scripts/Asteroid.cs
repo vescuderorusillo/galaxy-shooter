3c376b5b62d1b6d9ecc40921ef739daf00c18e6f
Victor Escudero <victor.escudero@bestsecret.com>
2021-02-02 18:58:24 +0100

    Created Explosion prefab

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
@@ -10,10 +8,15 @@ public class Asteroid : MonoBehaviour
     [SerializeField]
     private float _rotateSpeed = 20f;
 
+    [SerializeField] 
+    private GameObject _explosionPrefab;
+
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
         //transform.Translate(Vector3.down * _speed * Time.deltaTime);
     }
+
+    
 }
