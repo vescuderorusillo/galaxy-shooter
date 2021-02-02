e5995139807a5b053f8713c0e308a39d52abb8b1
Victor Escudero <victor.escudero@bestsecret.com>
2021-02-02 19:20:00 +0100

    Created Exploxion effect when the Asteroid collides with the Laser

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -2,21 +2,25 @@
 
 public class Asteroid : MonoBehaviour
 {
-    //[SerializeField]
-    //private float _speed = 2f;
-
     [SerializeField]
     private float _rotateSpeed = 20f;
 
     [SerializeField] 
     private GameObject _explosionPrefab;
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
-        //transform.Translate(Vector3.down * _speed * Time.deltaTime);
     }
 
-    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Laser"))
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+
+            Destroy(gameObject, 0.2f);
+            Destroy(other.gameObject);
+        }
+    }
 }
