9373c9615ae9c7d1f4cc139d47e8797102b5248d
Victor Escudero <victor.escudero@bestsecret.com>
2021-02-04 18:08:22 +0100

    Quit Application when ESC is pressed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,11 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     public void GameOver()
